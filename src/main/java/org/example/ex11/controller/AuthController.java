569d28d01a4984e0271df4bd1a2712449a5769d0
qus0in <qus0in@gmail.com>
2025-03-11 15:53:24 +0900

    push

--- src/main/java/org/example/ex11/controller/AuthController.java
+++ src/main/java/org/example/ex11/controller/AuthController.java
@@ -5,13 +5,21 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.Random;
 
 @WebServlet("/auth")
 public class AuthController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Random rand = new Random();
+        int luckyNumber = rand.nextInt(100);
+        HttpSession session = req.getSession();
+        if (session.getAttribute("lucky_number") == null) {
+            session.setAttribute("lucky_number", luckyNumber);
+        }
         req.getRequestDispatcher("/WEB-INF/auth.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/WEB-INF/auth.jsp
+++ src/main/webapp/WEB-INF/auth.jsp
@@ -5,6 +5,7 @@
     <title>세션과 쿠키</title>
 </head>
 <body>
-  <p>세션 좋아! 쿠키 좋아!</p>
+    <p>세션 좋아! 쿠키 좋아!</p>
+    <p>세션이 전하는 행운의 숫자 : <%= session.getAttribute("lucky_number")%></p>
 </body>
 </html>
